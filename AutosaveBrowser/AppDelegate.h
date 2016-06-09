8d0159f54bb79e8579a2eb75bce6525e07a473f7
Andy Gallagher <andy.gallagher@theguardian.com>
2016-06-09 16:03:30 +0100

    Initial Commit

---
+++ AutosaveBrowser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutosaveBrowser
+//
+//  Created by localhome on 09/06/2016.
+//  Copyright (c) 2016 Guardian News & Media. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AutosaveBrowser/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  AutosaveBrowser
+//
+//  Created by localhome on 09/06/2016.
+//  Copyright (c) 2016 Guardian News & Media. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
